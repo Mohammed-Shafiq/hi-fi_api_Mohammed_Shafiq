58d41e3192adcff3f015e2a1e60962df062209b3
Mohammed-Shafiq <31003552+Mohammed-Shafiq@users.noreply.github.com>
2017-12-06 14:59:55 +0100

    Ændring til webserver

--- config/sql.js
+++ config/sql.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 module.exports = {
     'connect': () => {
         return mysql.createConnection({
-            'host': 'localhost',
+            'host': '46.101.227.145',
             'user': 'root',
             'password': '',
             'database': 'hi-fi'
